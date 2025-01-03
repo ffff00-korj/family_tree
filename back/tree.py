3120fc13e3c8331b7ac8abc61355b32c5740ca79
Borovkov Ilya <iborovkov97@yandex.ru>
2025-01-03 19:58:55 +0300

    handler structure

--- back/tree.py
+++ back/tree.py
@@ -3,6 +3,21 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return '<p>Hello!</>'
+@app.route('login/')
+def login():
+    return '<p>login</p>'
+
+
+@app.route('tree/create/')
+def create_tree():
+    return '<p>create</p>'
+
+
+@app.route('tree/change/')
+def change_tree():
+    return '<p>change</p>'
+
+
+@app.route('tree/delete/')
+def delete_tree():
+    return '<p>delete</p>'
